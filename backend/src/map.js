4a0fc003829c66581cea723b3b6da101088d09b6
YosuaniBG <yborgesg@gmail.com>
2023-05-31 07:14:12 -0700

    Resumen
    Cambio total

--- backend/src/map.js
+++
@@ -1,9 +0,0 @@
-let map;
-
-function initMap(){
-    let ensenada = {lat: 31.8578, lng: -116.60631}
-    map= new google.maps.Map(document.getElementById("map"),{
-        center: ensenada,
-        zoom: 14
-    })
-}
